8a7c0ce1e157b48fc329a6dc422530cb12db4b6c
williambritodev12 <williambritodev@gmail.com>
2025-08-26 21:58:20 -0300

    Desenho Tela de Login

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -6,29 +6,56 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
-          children: [
-            Image.asset("assets/images/mulher_usando_o_banco.jpg"),
-            Padding(padding: EdgeInsets.all(50)),
-            Text(
-              "Sistema de Gestão de Contas",
-              style: TextStyle(fontSize: 32),
-              textAlign: TextAlign.center,
+      body: Stack(
+        children: [
+          Image.asset(
+            "assets/images/mulher_usando_o_banco.jpg",
+            color: const Color.fromARGB(68, 0, 0, 0),
+            colorBlendMode: BlendMode.darken,
+            fit: BoxFit.cover,
+            height: double.infinity,
+          ),
+          Padding(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Image.asset("assets/images/mulher_usando_o_banco.jpg"),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(padding: EdgeInsets.all(50)),
+                    Text(
+                      "Sistema de Gestão de Contas",
+                      style: TextStyle(fontSize: 32),
+                      textAlign: TextAlign.center,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(label: Text("E-mail")),
+                    ),
+                    Padding(padding: EdgeInsets.all(10)),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(label: Text("Senha: ")),
+                    ),
+                    SizedBox(height: 25),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(50),
+                    backgroundColor: Color.fromARGB(255, 2, 109, 231),
+                  ),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+              ],
             ),
-            TextFormField(decoration: InputDecoration(label: Text("E-mail"))),
-            Padding(padding: EdgeInsets.all(10)),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(label: Text("Senha: ")),
-            ),
-            SizedBox(height: 25),
-            ElevatedButton(onPressed: () {}, child: Text("Login")),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
